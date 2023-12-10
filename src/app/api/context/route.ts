d42dd1c28d2e0a0a612117a328cd86b8217779b8
Vinci Rufus <vinci.rufus@gmail.com>
2023-12-10 10:39:56 +0000

    twillio code to context / route

--- src/app/api/context/route.ts
+++ src/app/api/context/route.ts
@@ -2,11 +2,18 @@ import { NextResponse } from "next/server";
 import { getContext } from "@/utils/context";
 import { ScoredPineconeRecord } from "@pinecone-database/pinecone";
 
-export async function POST(req: Request) {
+export async function POST(req: any, res: any) {
+  const MessagingResponse = require('twilio').twiml.MessagingResponse; 
+  var messageResponse = new MessagingResponse();
   try {
     const { messages } = await req.json()
     const lastMessage = messages.length > 1 ? messages[messages.length - 1] : messages[0]
     const context = await getContext(lastMessage.content, '', 10000, 0.7, false) as ScoredPineconeRecord[]
+    messageResponse.context
+    res.writeHead(200, {
+      'Content-Type': 'text/xml' });
+      res.end(messageResponse.toString()); 
+    
     return NextResponse.json({ context })
   } catch (e) {
     console.log(e)
